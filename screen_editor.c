bc279c377eac6694e2894d6fe52e89e92a5f1cca
wooky <no@no>
2023-05-08 09:38:34 -0600

    Preserve settings after code generation

--- screen_editor.c
+++ screen_editor.c
@@ -9,20 +9,25 @@ static const char palette[] = {
   0x0f, 0x0f, 0x0f, 0x30,
 };
 static const uint8_t status_bar_nametable[32 * 3 + 1] =
-  "F1 ECL L F2 MASK 0 F3 bECL F    "
-  "F8 RUN PAGE 00 CHAR 0000/1465   "
+  "F1 ECL ? F2 MASK ? F3 bECL ?    "
+  "F8 RUN PAGE 00 CHAR 0000/????   "
   "________________________________"
   "\x7f"
   ;
 static const uint8_t ecl_values[4] = "LMQH";
 static const uint8_t bool_values[2] = "FT";
-static const char max_text_size[4][4] = {
+static const uint8_t max_text_size[4][4] = {
   "1465",
   "1125",
   "0805",
   "0625",
 };
+
+#define ECL_VRAM NTADR_A(7, 0)
+#define MASK_VRAM NTADR_A(17, 0)
+#define BECL_VRAM NTADR_A(27, 0)
 #define TEXT_SIZE_VRAM NTADR_A(20, 1)
+#define MAX_TEXT_SIZE_VRAM NTADR_A(25, 1)
 
 static uint8_t key, last_key;
 static uint8_t vram_buf[16];
@@ -34,7 +39,21 @@ static char *text_size_ptr;
 
 void main (void)
 {
+  ecl = qrcodegen_Ecc_LOW;
+  mask = qrcodegen_Mask_0;
+  boostEcl = false;
+
   screen_editor();
+}
+
+uint8_t _mask_char (uint8_t delta)
+{
+  mask += delta;
+  if (mask > qrcodegen_Mask_7)
+  {
+    mask = qrcodegen_Mask_AUTO;
+  }
+  return (mask == qrcodegen_Mask_AUTO) ? 'A' : mask + '0';
 }
 
 void screen_editor (void)
@@ -45,16 +64,21 @@ void screen_editor (void)
 
   vram_adr(NTADR_A(0, 0));
   vram_write(status_bar_nametable, sizeof(status_bar_nametable));
+  vram_adr(ECL_VRAM);
+  vram_put(ecl_values[ecl]);
+  vram_adr(MASK_VRAM);
+  vram_put(_mask_char(0));
+  vram_adr(BECL_VRAM);
+  vram_put(bool_values[boostEcl]);
+  vram_adr(MAX_TEXT_SIZE_VRAM);
+  vram_write(max_text_size[ecl], sizeof(max_text_size[0]));
+  vram_adr(NTADR_A(0, 0) + sizeof(status_bar_nametable));
   vram_fill(0, NAMETABLE_B - NAMETABLE_A - sizeof(status_bar_nametable));
 
   vram_ptr = NTADR_A(0, 3);
   vram_buf[0] = NT_UPD_EOF;
 
   buf_ptr = tempBuffer;
-  ecl = qrcodegen_Ecc_LOW;
-  mask = qrcodegen_Mask_0;
-  boostEcl = false;
-
   memfill(text_size, '0', sizeof(text_size));
 
   ppu_on_all();
@@ -75,11 +99,10 @@ void screen_editor (void)
         {
           ecl = 0;
         }
-        #define ECL_VRAM NTADR_A(7, 0)
+        
         vram_buf[0] = MSB(ECL_VRAM);
         vram_buf[1] = LSB(ECL_VRAM);
         vram_buf[2] = ecl_values[ecl];
-        #define MAX_TEXT_SIZE_VRAM NTADR_A(25, 1)
         vram_buf[3] = MSB(MAX_TEXT_SIZE_VRAM) | NT_UPD_HORZ;
         vram_buf[4] = LSB(MAX_TEXT_SIZE_VRAM);
         vram_buf[5] = sizeof(max_text_size[0]);
@@ -88,26 +111,14 @@ void screen_editor (void)
         break;
 
       case KEYBOARD_F2:
-        #define MASK_VRAM NTADR_A(17, 0)
         vram_buf[0] = MSB(MASK_VRAM);
         vram_buf[1] = LSB(MASK_VRAM);
+        vram_buf[2] = _mask_char(1);
         vram_buf[3] = NT_UPD_EOF;
-
-        ++mask;
-        if (mask > qrcodegen_Mask_7)
-        {
-          mask = qrcodegen_Mask_AUTO;
-          vram_buf[2] = 'A';
-        }
-        else
-        {
-          vram_buf[2] = mask + '0';
-        }
         break;
 
       case KEYBOARD_F3:
         boostEcl ^= 1;
-        #define BECL_VRAM NTADR_A(27, 0)
         vram_buf[0] = MSB(BECL_VRAM);
         vram_buf[1] = LSB(BECL_VRAM);
         vram_buf[2] = bool_values[boostEcl];
@@ -149,7 +160,7 @@ void screen_editor (void)
         break;
       
       default:
-        if (strcmp(text_size, max_text_size[ecl]) == 0)
+        if (strcmp(text_size, (const char*)max_text_size[ecl]) == 0)
         {
           break;
         }
